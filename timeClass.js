232ef89302d231bfbc2c0bb312816611fee4e833
omar2004712 <o.bahutair@gmail.com>
2022-05-27 19:58:50 +0400

    fixed starting issue when hitting the start button multiple times in a row several intervals are set fixed dial issue when hitting the continue button the dial starts from the beginning

--- timeClass.js
+++ timeClass.js
@@ -3,6 +3,7 @@ class Timer {
         this.durationInput = durationInput;
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
+        this.duration = 3;
         if(callBacks){
             this.onStart = callBacks.onStart;
             this.onTick = callBacks.onTick;
@@ -10,25 +11,33 @@ class Timer {
         }
         this.startBtn.addEventListener('click', this.start);
         this.pauseBtn.addEventListener('click', this.pause);
-        this.durationInput.addEventListener('click', this.changeDuration);
+        this.durationInput.addEventListener('change', this.changeDuration);
     }
     start = ()=>{
-        this.onStart(this.timeRamining);
-        this.tick();
-        this.intervalId = setInterval(this.tick, 10)
+        if(!this.status){
+            this.onStart(this.duration);
+            this.tick();
+            this.status = true; //true means started
+            this.intervalId = setInterval(this.tick, 10)
+        }
     }
     pause = ()=>{
         clearInterval(this.intervalId);
+        this.status = false;
     }
     tick = ()=>{
         if(this.timeRamining <= 0){
             this.pause();
             this.onComplete();
+            this.status = false;
         }
         else{
             this.timeRamining = this.timeRamining - 0.01;
             this.onTick(this.timeRamining);
         }
+    }
+    changeDuration = ()=>{
+        this.duration = this.timeRamining;
     }
     get timeRamining(){
         return this.durationInput.value;
